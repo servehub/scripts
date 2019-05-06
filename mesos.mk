c6da3c9e93372e7f85b8863c3ba5e856d6ba2d6f
kulikov <kulikov.dm@gmail.com>
2019-05-06 13:12:24 +0300

    makefile: fix mesos.mk - kill all tasks on maintained host (via marathon)

--- mesos.mk
+++ mesos.mk
@@ -1,32 +1,41 @@
-mesos_host ?= 'localhost'
+host ?= 'inf9'
+env_domain ?= 'qa.example.com'
 
 mesos-slave-down:
-	$(eval ip=`ping -c 1 ${host} | awk -F'[()]' '/PING/{print $$$$2}'`)
+	$(eval ip=`ping -c 1 ${host}.${env_domain} | awk -F'[()]' '/PING/{print $$$$2}'`)
+
 	@echo ' \
 		{ \
 		  "windows" : [{ \
-		    "machine_ids" : [{ "hostname" : "${host}", "ip": "'${ip}'" }], \
+		    "machine_ids" : [{ "hostname" : "${host}.${env_domain}", "ip": "'${ip}'" }], \
 		    "unavailability" : { \
 		      "start" : { "nanoseconds" : '`date +%s`'000000000 }, \
 		      "duration" : { "nanoseconds" : 3600000000000 } \
 		    } \
 		  }] \
-		}' | http -v POST http://${mesos_host}/maintenance/schedule "Content-Type: application/json"
+		}' | http -v POST http://mesos.${env_domain}/master/maintenance/schedule "Content-Type: application/json"
 
 	sleep 3
 
 	@echo ' \
 		[ \
-		  { "hostname" : "${host}", "ip": "'${ip}'" } \
-		]' | http -v POST http://${mesos_host}/machine/down "Content-Type: application/json"
+		  { "hostname" : "${host}.${env_domain}", "ip": "'${ip}'" } \
+		]' | http -v POST http://mesos.${env_domain}/machine/down "Content-Type: application/json"
 
-	@make ansible-shell cmd='docker stop compose_mesos-slave_1' filter="tag:Name=${host}"
+	sleep 1
+
+	@make ansible-shell cmd='docker stop compose_mesos-slave_1' filter="tag:Name=${host}.${env_domain}"
+
+	curl http://marathon.live.boople.co/v2/tasks \
+		| jq '{"ids": [.tasks[] | select(.host == "${host}.${env_domain}").id]}' \
+		| http -v POST http://marathon.${env_domain}/v2/tasks/delete
 
 mesos-slave-up:
-	$(eval ip=`ping -c 1 ${host} | awk -F'[()]' '/PING/{print $$$$2}'`)
+	$(eval ip=`ping -c 1 ${host}.${env_domain} | awk -F'[()]' '/PING/{print $$$$2}'`)
+
 	@echo ' \
 		[ \
-		  { "hostname" : "${host}", "ip": "'${ip}'" } \
-		]' | http -v POST http://${mesos_host}/machine/up "Content-Type: application/json"
+		  { "hostname" : "${host}.${env_domain}", "ip": "'${ip}'" } \
+		]' | http -v POST http://mesos.${env_domain}/machine/up "Content-Type: application/json"
 
-	@make ansible-shell cmd='docker start compose_mesos-slave_1' filter="tag:Name=${host}"
+	@make ansible-shell cmd='docker start compose_mesos-slave_1' filter="tag:Name=${host}.${env_domain}"
